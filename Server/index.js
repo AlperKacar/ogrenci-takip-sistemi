826126f4b2d5394ea9a1c6a09a80e42e843cb093
ertnercr <ertan.eracar@gmail.com>
2023-06-09 00:30:19 +0300

    Başlangıç
    Dosyaları Ayarlandı

---
+++ Server/index.js
@@ -0,0 +1,46 @@
+import express from "express"
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import mime from "mime-types";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(cookieParser());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+
+app.use("/admin", admins);
+app.use("/home", homeilan);
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("Server çalışıyor: http://localhost:3000")
+    );
+  })
+  .catch((error) => console.log(error));
